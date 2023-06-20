14f8c7b3c8389f6a75829f54247874e6bd4810b1
milkyway044 <milkyway044@users.noreply.github.com>
2023-06-20 06:27:41 +0300

    Refactor argparse and calculate duration for multiple CSV files
    The argparse component of the code has been refactored to accept multiple CSV files as input. Additionally, the duration calculation has been updated to handle multiple CSV files.

--- toggle_track_reports.py
+++ toggle_track_reports.py
@@ -6,9 +6,12 @@ from datetime import datetime, timedelta
 import argparse
 import calendar
 
-parser = argparse.ArgumentParser(description='Parse a CSV file and group the results')
-parser.add_argument('csv_file', type=str, help='path to the input CSV file')
+parser = argparse.ArgumentParser(
+    description='Parse a CSV file and group the results')
+parser.add_argument('csv_files', nargs='+', type=str,
+                    help='paths to the input CSV files')
 args = parser.parse_args()
+
 
 DAY_NAMES = {
     0: 'Понедельник',
@@ -35,48 +38,55 @@ MONTH_NAMES = {
     12: 'декабря'
 }
 
-with open(args.csv_file) as f:
-    reader = csv.DictReader(f)
-    tasks_by_day = defaultdict(list)
-    total_duration = 0.0
-    for row in reader:
-        start_time = datetime.strptime(row['Start date'] + row['Start time'], '%Y-%m-%d%H:%M:%S')
-        end_time = datetime.strptime(row['End date'] + row['End time'], '%Y-%m-%d%H:%M:%S')
-        duration = (end_time - start_time).total_seconds() / 3600  # calculate in hours
-        day = start_time.date()
-        task = row['Description']
-        tasks_by_day[day].append((task, duration))
-        total_duration += duration
+tasks_by_day = defaultdict(lambda: defaultdict(float))
+total_duration = 0.0
 
-    current_week = None
-    week_tasks = []
-    week_start = week_end = None
-    for day, tasks in sorted(tasks_by_day.items()):
-        week_number = day.isocalendar()[1]
-        if week_number != current_week:
-            if current_week is not None:
-                week_total = sum(task[1] for task in week_tasks)
-                print("**Неделя {0} ({1}—{2} {3})** `{4:.1f}h`".format(current_week, week_start.day, week_end.day, MONTH_NAMES[week_start.month], round(week_total, 1)))
-                print("___________________\n")
-            week_tasks = []
-            current_week = week_number
-            week_start = day
+for csv_file in args.csv_files:
+    with open(csv_file) as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            start_time = datetime.strptime(
+                row['Start date'] + row['Start time'], '%Y-%m-%d%H:%M:%S')
+            end_time = datetime.strptime(
+                row['End date'] + row['End time'], '%Y-%m-%d%H:%M:%S')
+            duration = (end_time - start_time).total_seconds() / \
+                3600  # calculate in hours
+            day = start_time.date()
+            task = (row['Project'], row['Description'])
+            tasks_by_day[day][task] += duration
+            total_duration += duration
 
-        week_end = day
-        week_tasks.extend(tasks)
-        
-        grouped_tasks = defaultdict(float)
-        for task, duration in tasks:
-            grouped_tasks[task] += duration
+current_week = None
+week_tasks = []
+week_start = week_end = None
+for day, tasks in sorted(tasks_by_day.items()):
+    week_number = day.isocalendar()[1]
+    if week_number != current_week:
+        if current_week is not None:
+            week_total = sum(task[1] for task in week_tasks)
+            print("**Неделя {0} ({1}—{2} {3})** `{4:.1f}h`\n".format(current_week, week_start.day,
+                                                                     week_end.day, MONTH_NAMES[week_start.month], round(week_total, 1)))
+            print("___________________\n")
+        week_tasks = []
+        current_week = week_number
+        week_start = day
 
-        day_total = sum(grouped_tasks.values())
-        day_name = DAY_NAMES[day.weekday()]
-        month_name = MONTH_NAMES[day.month]
-        print("📆 **{0}, {1} {2}** - `{3:.1f}h`".format(day_name, day.day, month_name, round(day_total, 1)))
-        for task, duration in grouped_tasks.items():
-            print("- {0}: `{1:.1f}h`".format(task, round(duration, 1)))
+    week_end = day
+    week_tasks.extend(tasks.items())
 
-    week_total = sum(task[1] for task in week_tasks)
-    print("**Неделя {0} ({1}—{2} {3})** `{4:.1f}h`".format(current_week, week_start.day, week_end.day, MONTH_NAMES[week_start.month], round(week_total, 1)))
-    print("___________________")
-    print("📊 **Общее время:** `{0:.1f}h`".format(round(total_duration, 1)))
+    day_total = sum(tasks.values())
+    day_name = DAY_NAMES[day.weekday()]
+    month_name = MONTH_NAMES[day.month]
+    print("📆 **{0}, {1} {2}** - `{3:.1f}h`".format(day_name,
+                                                   day.day, month_name, round(day_total, 1)))
+    for (project, task), duration in sorted(tasks.items(), key=lambda x: x[1], reverse=True):
+        print(
+            "- #{0}: {1}: `{2:.1f}h`".format(project, task, round(duration, 1)))
+    print("\n")  # New line after each day
+
+
+week_total = sum(duration for _, duration in week_tasks)
+print("**Неделя {0} ({1}—{2} {3})** `{4:.1f}h`\n".format(current_week, week_start.day,
+                                                         week_end.day, MONTH_NAMES[week_start.month], round(week_total, 1)))
+print("___________________")
+print("📊 **Общее время:** `{0:.1f}h`".format(round(total_duration, 1)))
